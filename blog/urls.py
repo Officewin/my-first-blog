36b228ca887f6cad518b73f2087b2e998518b38f
Officewin <104660484+Officewin@users.noreply.github.com>
2025-07-06 10:30:22 +0700

    Add Interview page with Vue component

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('interview/', views.interview, name='interview'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+
+def interview(request):
+    """Render the interview preparation page."""
+    return render(request, 'blog/interview.html', {})
